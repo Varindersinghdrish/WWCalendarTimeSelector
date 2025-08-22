7f11956b41412b8920630bbec2a540652d921c36
Varindersinghdrish <varindersinghdrish@gmail.com>
2025-08-22 12:38:31 +0530

    Update Package.swift
    fixes

--- Package.swift
+++ Package.swift
@@ -16,12 +16,42 @@ let package = Package(
         .target(
             name: "WWCalendarTimeSelector",
             dependencies: [],
-            path: "WWCalendarTimeSelector",   // where your .swift files are
+            path: "WWCalendarTimeSelector",   // folder that contains .swift files
             exclude: ["Info.plist", "WWCalendarTimeSelector.h"],
             resources: [
-                .process("../Resources")      // resources folder next to code folder
+                .process("Resources")        // folder inside WWCalendarTimeSelector/
             ]
         )
     ],
     swiftLanguageVersions: [.v5]
 )
+
+
+
+// // swift-tools-version:5.3
+// import PackageDescription
+
+// let package = Package(
+//     name: "WWCalendarTimeSelector",
+//     platforms: [
+//         .iOS(.v11)
+//     ],
+//     products: [
+//         .library(
+//             name: "WWCalendarTimeSelector",
+//             targets: ["WWCalendarTimeSelector"]
+//         )
+//     ],
+//     targets: [
+//         .target(
+//             name: "WWCalendarTimeSelector",
+//             dependencies: [],
+//             path: "WWCalendarTimeSelector",   // where your .swift files are
+//             exclude: ["Info.plist", "WWCalendarTimeSelector.h"],
+//             resources: [
+//                 .process("../Resources")      // resources folder next to code folder
+//             ]
+//         )
+//     ],
+//     swiftLanguageVersions: [.v5]
+// )
