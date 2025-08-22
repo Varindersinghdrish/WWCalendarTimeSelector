44f2e53ab5bc4b8e56ed906fe4de582da3c9f6c1
Bhumika Sharma <bhumika.sharma@drishinfo.com>
2025-08-22 12:52:19 +0530

    SPM: point target to Sources

--- Package.swift
+++ Package.swift
@@ -3,26 +3,49 @@ import PackageDescription
 
 let package = Package(
     name: "WWCalendarTimeSelector",
-    platforms: [
-        .iOS(.v11)
-    ],
+    platforms: [.iOS(.v11)],
     products: [
-        .library(
-            name: "WWCalendarTimeSelector",
-            targets: ["WWCalendarTimeSelector"]
-        )
+        .library(name: "WWCalendarTimeSelector", targets: ["WWCalendarTimeSelector"])
     ],
     targets: [
         .target(
             name: "WWCalendarTimeSelector",
-            dependencies: [],
-            path: "Sources/WWCalendarTimeSelector",
-            exclude: ["Info.plist"],
+            path: "Sources",
             resources: [
-                .process("WWCalendarTimeSelector.storyboard"),
-                .process("Assets.xcassets")
+                .process("WWCalendarTimeSelector.storyboard")
             ]
         )
     ],
     swiftLanguageVersions: [.v5]
 )
+
+
+
+//// swift-tools-version:5.3
+//import PackageDescription
+//
+//let package = Package(
+//    name: "WWCalendarTimeSelector",
+//    platforms: [
+//        .iOS(.v11)
+//    ],
+//    products: [
+//        .library(
+//            name: "WWCalendarTimeSelector",
+//            targets: ["WWCalendarTimeSelector"]
+//        )
+//    ],
+//    targets: [
+//        .target(
+//            name: "WWCalendarTimeSelector",
+//            dependencies: [],
+//            path: "Sources/WWCalendarTimeSelector",
+//            exclude: ["Info.plist"],
+//            resources: [
+//                .process("WWCalendarTimeSelector.storyboard"),
+//                .process("Assets.xcassets")
+//            ]
+//        )
+//    ],
+//    swiftLanguageVersions: [.v5]
+//)
